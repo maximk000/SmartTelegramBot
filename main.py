11ed9a3ea92acbc34ee7633d112da47a98804f72
Maxim K <maxim.mit.glg@gmail.com>
2021-06-19 01:21:33 +0300

    Conversation handler started

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+# Project files
 import constants
-from telegram.ext import Updater
+import manual_utterances
+# Python libraries
 import logging
+import numpy as np
+# telegram-python-bot api
+from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 from telegram.ext import InlineQueryHandler
 from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import ConversationHandler, CallbackQueryHandler
 
 API_TOKEN = constants.telegram_api_key
 
@@ -14,8 +20,17 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                      level=logging.INFO)
 
 
+
+
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    rand_idx = np.random.randint(len(manual_utterances.directed_greeting))
+    msg_text = "{} {}. I'm a bot, please talk to me!".format(manual_utterances.directed_greeting[rand_idx],
+                                                             update.message.from_user.first_name)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=msg_text)
+
+
+def help(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=msg_text)
 
 
 def echo(update, context):
@@ -24,6 +39,9 @@ def echo(update, context):
 
 
 def caps(update, context):
+    print(update)
+    print('----')
+    print(context)
     text_caps = ' '.join(context.args).upper()
     context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
@@ -65,6 +83,78 @@ def main():
     unknown_handler = MessageHandler(Filters.command, unknown)
     dispatcher.add_handler(unknown_handler)
 
+
+    description_conv = ConversationHandler(
+        entry_points=[
+            CallbackQueryHandler(
+                select_feature, pattern='^' + str(MALE) + '$|^' + str(FEMALE) + '$'
+            )
+        ],
+        states={
+            SELECTING_FEATURE: [
+                CallbackQueryHandler(ask_for_input, pattern='^(?!' + str(END) + ').*$')
+            ],
+            TYPING: [MessageHandler(Filters.text & ~Filters.command, save_input)],
+        },
+        fallbacks=[
+            CallbackQueryHandler(end_describing, pattern='^' + str(END) + '$'),
+            CommandHandler('stop', stop_nested),
+        ],
+        map_to_parent={
+            # Return to second level menu
+            END: SELECTING_LEVEL,
+            # End conversation altogether
+            STOPPING: STOPPING,
+        },
+    )
+
+    # Set up second level ConversationHandler (adding a person)
+    add_member_conv = ConversationHandler(
+        entry_points=[CallbackQueryHandler(select_level, pattern='^' + str(ADDING_MEMBER) + '$')],
+        states={
+            SELECTING_LEVEL: [
+                CallbackQueryHandler(select_gender, pattern=f'^{PARENTS}$|^{CHILDREN}$')
+            ],
+            SELECTING_GENDER: [description_conv],
+        },
+        fallbacks=[
+            CallbackQueryHandler(show_data, pattern='^' + str(SHOWING) + '$'),
+            CallbackQueryHandler(end_second_level, pattern='^' + str(END) + '$'),
+            CommandHandler('stop', stop_nested),
+        ],
+        map_to_parent={
+            # After showing data return to top level menu
+            SHOWING: SHOWING,
+            # Return to top level menu
+            END: SELECTING_ACTION,
+            # End conversation altogether
+            STOPPING: END,
+        },
+    )
+
+    # Set up top level ConversationHandler (selecting action)
+    # Because the states of the third level conversation map to the ones of the second level
+    # conversation, we need to make sure the top level conversation can also handle them
+    selection_handlers = [
+        add_member_conv,
+        CallbackQueryHandler(show_data, pattern='^' + str(SHOWING) + '$'),
+        CallbackQueryHandler(adding_self, pattern='^' + str(ADDING_SELF) + '$'),
+        CallbackQueryHandler(end, pattern='^' + str(END) + '$'),
+    ]
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            SHOWING: [CallbackQueryHandler(start, pattern='^' + str(END) + '$')],
+            SELECTING_ACTION: selection_handlers,
+            SELECTING_LEVEL: selection_handlers,
+            DESCRIBING_SELF: [description_conv],
+            STOPPING: [CommandHandler('start', start)],
+        },
+        fallbacks=[CommandHandler('stop', stop)],
+    )
+
+    dispatcher.add_handler(conv_handler)
+
     # Start the bot
     updater.start_polling()
 
@@ -74,3 +164,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
