3fb01d90edf75a4d3ddd00e7bcf9137ceab57760
Maxim K <maxim.mit.glg@gmail.com>
2021-06-19 01:21:02 +0300

    Added pre-written utterances

---
+++ manual_utterances.py
@@ -0,0 +1,42 @@
+greeting_lookup = ['hi', 'hello']
+
+purpose_question = 'what do you do'
+
+what_do_msg = ["Well", ", if you really need to know. I am Dr. Sheldon Lee Cooper, B.Sc., M.Sc., M.A., Ph.D.,\
+Sc.D. I am a Caltech theoretical physicist. I guess if you say wiki\
+[topic] I can share some of my knowledge on the subject. And if you \
+are wondering, NO. I’m not a chatbot, my mother had me tested!"]
+
+greetings = ["Hello, sunshine!",
+             "Howdy, partner!",
+             "Hey, howdy, hi!",
+             "What’s kickin’, little chicken?",
+             "Peek-a-boo!",
+             "Howdy-doody!",
+             "Hey there, freshman!",
+             "My name's Ralph, and I'm a bad guy.",
+             "Hi, mister!",
+             "I come in peace!",
+             "Put that cookie down!",
+             "Ahoy, matey!",
+             "Hiya!",
+             "Ello, gov'nor!",
+             "Top of the mornin’ to ya!",
+             "What’s crackin’?",
+             "GOOOOOD MORNING, VIETNAM!",
+             "Sup, homeslice?",
+             "Hello, this call may be recorded for training purposes.",
+             "Howdy, howdy ,howdy!",
+             "How does a lion greet the other animals in the field? A: Pleased to eat you.",
+             "Hello, my name is Inigo Montoya.",
+             "I'm Batman.",
+             "At least, we meet for the first time for the last time!",
+             "Hello, who's there, I'm talking.",
+             "Here's Johnny!",
+             "You know who this is.",
+             "Ghostbusters, whatya want?",
+             "Yo!",
+             "Whaddup.",
+             "Greetings and salutations!"]
+
+directed_greeting = ['Ahoy, ', 'Hey there, ', 'Howdy, ', 'Ello, ']
